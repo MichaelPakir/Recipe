b89244910ed4cbe16f98900eb0cc0116d8288677
MichaelPakir <neovimm@gmail.com>
2025-03-20 16:36:37 +0800

    Active

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/sidebar/sidebar.css";
 
 const Sidebar = ({ navLinks }) => {
+  const [activeLink, setActiveLink] = useState("");
+
+  const handleLinkClick = (label) => {
+    setActiveLink(label);
+  };
+
   return (
     <div className="sidebar">
       <nav className="sidebar__nav">
@@ -19,7 +25,13 @@ const Sidebar = ({ navLinks }) => {
           <div className="nav__container">
             {navLinks.map((link, index) => (
               <div className="nav__item" key={index}>
-                <Link to={link.path} className="nav__link">
+                <Link
+                  to={link.path}
+                  className={`nav__link ${
+                    activeLink === link.label ? "active__work" : ""
+                  }`}
+                  onClick={() => handleLinkClick(link.label)}
+                >
                   <span className="nav__icon">{link.icon}</span>
                   <span className="nav__text">{link.label}</span>
                 </Link>
