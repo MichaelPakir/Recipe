702b4adae729efacfa2060560d43b3123a7b0f94
MichaelPakir <neovimm@gmail.com>
2025-05-08 21:53:19 +0800

    Sidebar Changes

--- src/components/SidebarMobile.jsx
+++ src/components/SidebarMobile.jsx
@@ -1,30 +1,36 @@
 import React, { useState } from "react";
 import { Divide as Hamburger } from "hamburger-react";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
+import "../styles/sidebar/sidebar.css";
 
 const SidebarMobile = ({ navLinks }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="sidebar--mobile">
-      <div>
-        <Hamburger Divide toggled={isOpen} toggle={setIsOpen} />
+      <div className="burger__icon">
+        <Hamburger toggled={isOpen} toggle={setIsOpen} />
       </div>
 
       {isOpen && (
         <div className="mobile__overlay">
-          <nav className="mobile__nav">
-            {navLinks.map((link, index) => (
-              <Link
-                to={link.path}
-                key={index}
-                onClick={() => setIsOpen(false)}
-                className="nav__container"
-              >
-                <span className="nav__text">{link.label}</span>
-              </Link>
-            ))}
-          </nav>
+          <div className="mobile__content">
+            <div className="mobile__logo">
+              <h2>LOGO</h2>
+            </div>
+            <nav className="mobile__nav">
+              {navLinks.map((link, index) => (
+                <Link
+                  to={link.path}
+                  key={index}
+                  onClick={() => setIsOpen(false)}
+                  className="nav__container"
+                >
+                  <span className="nav__text">{link.label}</span>
+                </Link>
+              ))}
+            </nav>
+          </div>
         </div>
       )}
     </div>
